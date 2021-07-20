f197be773d7a6060c9ce381053e97efc10882991
trapcodeio <trapcode.io@gmail.com>
2021-07-20 13:43:02 +0100

    Added setRef and setRefFrom

--- vue3/ref-kit.ts
+++ vue3/ref-kit.ts
@@ -1,10 +1,42 @@
 import {Ref} from "vue";
 
 /**
+ * Sets ref value
+ *
+ * @example
+ * setRef(ref, "value")
+ * // is same as
+ * ref.value = "value";
+ *
+ * @param ref
+ * @param value
+ */
+export function setRef<T = any>(ref: Ref<T>, value: T) {
+    ref.value = value;
+    return ref.value;
+}
+
+
+/**
+ * Sets ref value to the result of the function passed.
+ *
+ * @example
+ * setRefFrom(ref, (v) => v ? "yes": "no")
+ * // is same as
+ * ref.value = ref.value ? "yes": "no";
+ * @param ref
+ * @param from
+ */
+export function setRefFrom<T = any>(ref: Ref<T>, from: (value: T) => T) {
+    return setRef(ref, from(ref.value));
+}
+
+/**
  * Toggles a ref.
  * @param ref
  * @param set
  */
+
 export function toggleRef(ref: Ref<boolean>, set?: boolean) {
     if (typeof set === "boolean") {
         ref.value = set;
@@ -21,7 +53,7 @@ export function toggleRef(ref: Ref<boolean>, set?: boolean) {
  * @param set
  */
 export function toggleRefFn(ref: Ref<boolean>, set?: boolean) {
-    return ($set?: boolean | void) => toggleRef(ref, typeof $set === "boolean"  ? $set : set);
+    return ($set?: boolean | void) => toggleRef(ref, typeof $set === "boolean" ? $set : set);
 }
 
 /**
