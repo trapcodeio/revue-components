5a8e450474616052106eb706401c81dee69d0260
trapcodeio <trapcode.io@gmail.com>
2021-07-20 10:14:44 +0100

    Add Vu3 ref kits

---
+++ vue3/ref-kit.ts
@@ -0,0 +1,44 @@
+import { Ref } from "vue";
+
+/**
+ * Toggles a ref.
+ * @param ref
+ * @param set
+ */
+export function toggleRef(ref: Ref<boolean>, set?: boolean) {
+    if (typeof set === "boolean") {
+        ref.value = set;
+    } else {
+        ref.value = !ref.value;
+    }
+
+    return ref.value;
+}
+
+/**
+ * Returns a function that toggles a ref
+ * @param ref
+ */
+export function toggleRefFn(ref: Ref<boolean>) {
+    return (set?: boolean) => toggleRef(ref, set);
+}
+
+/**
+ * Toggles a boolean in a reactive object.
+ * @param ref
+ * @param key
+ * @param set
+ */
+export function toggleInReactive<T = Record<any, any>>(
+    ref: T,
+    key: keyof T,
+    set?: boolean
+) {
+    if (typeof set === "boolean") {
+        (ref as any)[key] = set;
+    } else {
+        (ref as any)[key] = !ref[key];
+    }
+
+    return ref[key];
+}
---
+++ vue3/utils.ts
@@ -0,0 +1,40 @@
+import {ref} from "vue";
+
+export const isProd = (() => {
+    try {
+        // @ts-ignore
+        if (import.meta && import.meta.env) {
+            // @ts-ignore
+            return import.meta.env.PROD === true;
+        } else {
+            // @ts-ignore
+            if (process && process.env) {
+                // @ts-ignore
+                return process.env.NODE_ENV === "production";
+            } else return false;
+        }
+    } catch {
+        return false;
+    }
+})();
+
+export const isDev = !isProd;
+
+let showDevValues = ref(true);
+
+export function ifDev<T = any>(yes: T, no?: T) {
+    return isDev && showDevValues.value ? yes : no;
+}
+
+export function turnOffDevValues() {
+    showDevValues.value = false;
+}
+
+export function ifDevRun<T = any>(fn: () => T) {
+    if (isDev) return fn();
+}
+
+export function ifProdRun<T = any>(fn: () => T) {
+    if (!isDev) return fn();
+}
+
