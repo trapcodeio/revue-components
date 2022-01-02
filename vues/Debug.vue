fdf1d96849c5f9fd225e055ca7a35d69accbcfb1
trapcodeio <trapcode.io@gmail.com>
2022-01-02 10:47:16 +0100

    Add Debug Script

---
+++ vues/Debug.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+import {isDev} from "../vue3/utils";
+
+export default defineComponent({
+  props: ["data"],
+
+  setup(props) {
+    return {isDev}
+  }
+});
+</script>
+
+<template>
+  <pre v-if="isDev" class="text-xs my-10" v-text="data"></pre>
+</template>
+
