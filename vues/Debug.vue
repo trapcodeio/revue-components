cc7bc29e3c79660a0fba43da31c428627f579664
trapcodeio <trapcode.io@gmail.com>
2022-01-02 11:16:21 +0100

    Add process data for debug

--- vues/Debug.vue
+++ vues/Debug.vue
@@ -2,18 +2,27 @@
 import {defineComponent} from "vue";
 import {isDev} from "../vue3/utils";
 
-export default defineComponent({
-  props: ["data"],
-
-  setup(props) {
-    return {isDev}
+// Convert object to json string
+export function processData(data: any, space: number = 2) {
+  if (typeof data === "object") {
+    return JSON.stringify(data, null, space);
+  } else {
+    return data;
   }
+}
+
+export default defineComponent({
+  props: ["data", "space"],
+
+  setup() {
+    return {isDev, processData}
+  },
 });
 </script>
 
 <template>
   <pre v-if="isDev" class="text-xs my-10">
-    {{ data || undefined }}
+    {{ data ? processData(data, Number(space)) : undefined }}
 
     <slot></slot>
   </pre>
